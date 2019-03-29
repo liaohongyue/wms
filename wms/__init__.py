606aaae7ea9bba613517d1b5cb2fda462648b1da
liaohy <970071213@qq.com>
2019-03-29 17:27:10 +0800

    tmp

--- wms/__init__.py
+++ wms/__init__.py
@@ -9,9 +9,18 @@ from wms.blueprints.admin.user.user import user_bp
 
 from  wms.forms.test import LoginForm
 
+from wms.models import db
+
+import pymysql
+
+db.init_app(app)
+
+
+
 app = Flask('wms')
 
 # config app
+app.config.from_pyfile('config.py')
 app.secret_key='secret key'
 
 # register buleprint
@@ -21,3 +30,4 @@ app.register_blueprint(analysis_bp)
 app.register_blueprint(home_bp)
 app.register_blueprint(samples_bp)
 app.register_blueprint(user_bp)
+
---
+++ wms/config.py
@@ -0,0 +1,4 @@
+
+from wms import app
+
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://test:test123@localhost/test'
